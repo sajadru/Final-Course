92647d8c1543e579c4e40d78505a4e756c022df9
sajadru <salad.ru@gmail.com>
2019-05-01 01:37:39 +0430

    none

---
+++ Scroll-Demo/WebApp/Scripts/home.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+
+(function () {
+
+    $(function () {
+        $('#load-news').click(function () {
+            $('#top-news').load('news.html');
+        });
+    });
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+       
+        if (scroll >= 550) {
+
+            $('#top-news').load('news.html');
+               
+        }
+    });
+})();
